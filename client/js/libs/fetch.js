314ab27aaab6e0209e17f1f29d9a2eb253aff4b6
wangyichen1064431086 <1064431086@qq.com>
2018-09-13 16:40:24 +0800

    Some modify

--- client/js/libs/fetch.js
+++ client/js/libs/fetch.js
@@ -51,7 +51,7 @@ function fetchMoreInfoOfStorysNew(storyIdArr, cbFunc) {
   while(storyIdArr.length > 0) {
     const storyIdArr50 = storyIdArr.splice(0, 50);
     //TODO: http，线上地址是https，故会产生Mixed Content错误。
-    const requestUrl = 'http://10.99.10.11/index.php/jsapi/storyid2title/' + encodeURIComponent(storyIdArr50.join(','));
+    const requestUrl = 'https://www.ftchinese.com/index.php/jsapi/storyid2title/' + encodeURIComponent(storyIdArr50.join(','));
     console.log(requestUrl);
     fetch(requestUrl, {
       mode: 'cors'
